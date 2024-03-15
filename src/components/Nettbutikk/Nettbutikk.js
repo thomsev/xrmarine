0d9cd747277032b31ea58ea1a731a6526f10291d
thomsev <thomas_S@live.no>
2024-03-15 08:16:10 +0100

    css updates

--- src/components/Nettbutikk/Nettbutikk.js
+++ src/components/Nettbutikk/Nettbutikk.js
@@ -1,28 +1,29 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import "bootstrap/dist/css/bootstrap.min.css"; // Make sure to import Bootstrap CSS
 import "./Nettbutikk.css"; // Import custom CSS for Nettbutikk component
 import products from "../Assets/products";
 
 const Nettbutikk = () => {
   return (
-    <div className="container mt-5">
-      <div className="row">
-        {products.map((product) => (
-          <div className="col-md-4 mb-4" key={product.id}>
-            <Card className="h-100 product-card">
-              <img
-                src={require(`../Assets/${product.imgSrc}`)}
-                className="card-img-top"
-                alt={product.altText}
-              />
-              <Card.Body>
-                <Card.Title>{product.title}</Card.Title>
-                <Card.Text>{product.price}</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+    <div className="background-image-wrapper">
+      <div className="container mt-5">
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-md-4 mb-4" key={product.id}>
+              <Card className="h-100 product-card">
+                <img
+                  src={require(`../Assets/${product.imgSrc}`)}
+                  className="card-img-top"
+                  alt={product.altText}
+                />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>{product.price}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
